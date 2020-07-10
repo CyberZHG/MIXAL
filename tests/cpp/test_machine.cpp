06f39a645b41d0fcae5abf24afacf7b9ad27f092
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2020-07-10 22:43:01 +0800

    Add comments for machine.

---
+++ tests/cpp/test_machine.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "test.h"
+#include "machine.h"
+#include "parser.h"
+
+namespace test {
+
+class TestMachine : public UnitTest {
+ public:
+    mixal::Machine machine;
+
+    friend mixal::Machine;
+
+    TestMachine() : machine() {}
+
+    void setUpEach() final {
+        machine.reset();
+    }
+};
+
+__TEST_U(TestMachine, test_get_invalid_index_register) {
+    __ASSERT_THROW(machine.rI(0), mixal::RuntimeError);
+}
+
+__TEST_U(TestMachine, test_get_memory) {
+    __ASSERT_EQ(0, machine.memoryAt(0).value());
+}
+
+__TEST_U(TestMachine, test_get_memory_const) {
+    const auto& mac = machine;
+    __ASSERT_EQ(0, mac.memoryAt(0).value());
+}
+
+__TEST_U(TestMachine, test_line_output_of_range_self_loop) {
+    __ASSERT_THROW(machine.executeUntilSelfLoop(), mixal::RuntimeError);
+}
+
+__TEST_U(TestMachine, test_line_output_of_range_halt) {
+    __ASSERT_THROW(machine.executeUntilHalt(), mixal::RuntimeError);
+}
+
+}  // namespace test
